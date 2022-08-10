b7a18ba701798cedecd59ec74bd6339e97dcda6e
unknown <bkarshehbazgithub@gmail.com>
2022-08-10 14:12:57 +0500

    added final changes

--- src/features/timer/Timer.js
+++ src/features/timer/Timer.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { useInterval } from '../../components/control/useInterval';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useInterval } from "../../components/control/useInterval";
 import {
   incrementSeconds,
   decrementSeconds,
@@ -11,15 +11,12 @@ import {
   selectTimerActive,
   setTimerActive,
   resetTime,
-  updateInitialTime
-} from './timerSlice';
+  updateInitialTime,
+} from "./timerSlice";
 
-export default function Timer({
-  ascending,
-  extraTime,
-}) {
+export default function Timer({ ascending, extraTime }) {
   const dispatch = useDispatch();
-  const padNumber = (number) => number.toString().padStart(2, '0');
+  const padNumber = (number) => number.toString().padStart(2, "0");
 
   const minutes = useSelector(selectMinutes);
   const seconds = useSelector(selectSeconds);
@@ -33,13 +30,17 @@ export default function Timer({
     }
   }, 1000);
 
-
   const timeButton = (time, index) => (
-    <button key={index} type="button" className="btn btn-primary btn-sm btn-extra-time col-2" onClick={() => {
-      dispatch(updateInitialTime(time));
-      dispatch(updateMinutes(time));
-      dispatch(updateSeconds(0));
-    }}>
+    <button
+      key={index}
+      type="button"
+      className="btn btn-primary btn-sm btn-extra-time col-2"
+      onClick={() => {
+        dispatch(updateInitialTime(time));
+        dispatch(updateMinutes(time));
+        dispatch(updateSeconds(0));
+      }}
+    >
       {time}:{padNumber(0)}
     </button>
   );
@@ -50,33 +51,90 @@ export default function Timer({
         <h5>Tiempo</h5>
       </div>
       {
-          <div>
-            <div className="card-body timer-body">
-              <button type="button" className="btn btn-primary btn-sm btn-time" onClick={() => dispatch(decrementSeconds(60))}>-60</button>
-              <button type="button" className="btn btn-primary btn-sm btn-time" onClick={() => dispatch(decrementSeconds(10))}>-10</button>
-              <button type="button" className="btn btn-primary btn-sm btn-time" onClick={() => dispatch(decrementSeconds(1))}>-1</button>
-              <span className="time">{padNumber(minutes)}:{padNumber(seconds)}</span>
-              <button type="button" className="btn btn-primary btn-sm btn-time" onClick={() => dispatch(incrementSeconds(1))}>+1</button>
-              <button type="button" className="btn btn-primary btn-sm btn-time" onClick={() => dispatch(incrementSeconds(10))}>+10</button>
-              <button type="button" className="btn btn-primary btn-sm btn-time" onClick={() => dispatch(incrementSeconds(60))}>+60</button>
+        <div>
+          <div className="card-body timer-body top-bottom-buttons timer-wrapper-parent">
+            <div className="buttons-wrapper">
+              <button
+                type="button"
+                className="btn btn-primary btn-sm btn-time"
+                onClick={() => dispatch(decrementSeconds(60))}
+              >
+                -60
+              </button>
+              <button
+                type="button"
+                className="btn btn-primary btn-sm btn-time"
+                onClick={() => dispatch(decrementSeconds(10))}
+              >
+                -10
+              </button>
+              <button
+                type="button"
+                className="btn btn-primary btn-sm btn-time"
+                onClick={() => dispatch(decrementSeconds(1))}
+              >
+                -1
+              </button>
             </div>
-            <div className="row justify-content-center">
-              {
-                extraTime ?
 
-                  extraTime.map((time, index) => timeButton(time, index))
-                  :
-                  null
-              }
-            </div>
-            <div className="row justify-content-center">
-              <button type="button" className="btn btn-primary btn-sm btn-time-control col-4" onClick={() => dispatch(setTimerActive(true))} >Iniciar</button>
-              <button type="button" className="btn btn-primary btn-sm btn-time-control col-4" onClick={() => dispatch(setTimerActive(false))} >Pausa</button>
-              <button type="button" className="btn btn-primary btn-sm btn-time-control col-4" onClick={() => {
-                dispatch(resetTime())
-              }} >Reset</button>
+            <span className="time">
+              {padNumber(minutes)}:{padNumber(seconds)}
+            </span>
+            <div className="buttons-wrapper two">
+              <button
+                type="button"
+                className="btn btn-primary btn-sm btn-time"
+                onClick={() => dispatch(incrementSeconds(1))}
+              >
+                +1
+              </button>
+              <button
+                type="button"
+                className="btn btn-primary btn-sm btn-time"
+                onClick={() => dispatch(incrementSeconds(10))}
+              >
+                +10
+              </button>
+              <button
+                type="button"
+                className="btn btn-primary btn-sm btn-time"
+                onClick={() => dispatch(incrementSeconds(60))}
+              >
+                +60
+              </button>
             </div>
           </div>
+          <div className="row justify-content-center">
+            {extraTime
+              ? extraTime.map((time, index) => timeButton(time, index))
+              : null}
+          </div>
+          <div className="row justify-content-center">
+            <button
+              type="button"
+              className="btn btn-primary btn-sm btn-time-control col-4"
+              onClick={() => dispatch(setTimerActive(true))}
+            >
+              Iniciar
+            </button>
+            <button
+              type="button"
+              className="btn btn-primary btn-sm btn-time-control col-4"
+              onClick={() => dispatch(setTimerActive(false))}
+            >
+              Pausa
+            </button>
+            <button
+              type="button"
+              className="btn btn-primary btn-sm btn-time-control col-4"
+              onClick={() => {
+                dispatch(resetTime());
+              }}
+            >
+              Reset
+            </button>
+          </div>
+        </div>
       }
     </div>
   );
